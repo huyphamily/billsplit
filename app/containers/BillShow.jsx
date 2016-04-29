849c50a38e6713d401273d8f571f194f37c7221a
huyphamily <huyphamily@gmail.com>
2016-04-28 21:15:42 -0700

    add BillShow

--- app/containers/BillShow.jsx
+++ app/containers/BillShow.jsx
@@ -8,14 +8,77 @@ class BillShow extends Component {
     fetchBills
   ]
 
+  renderCreditTable() {
+    const creditView = this.props.bill.Credits.map((value) => {
+      return (
+        <tr key={value.id}>
+          <td>{value.description}</td>
+          <td>{value.amount}</td>
+        </tr>);
+    });
+
+    return (
+      <div>
+        <h3>Credits</h3>
+        <table className="table table-inverse">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {creditView}
+          </tbody>
+        </table>
+      </div>)
+  }
+
+  renderDebtTable() {
+    const debtView = this.props.bill.Debts.map((value) => {
+      return (
+        <tr key={value.id}>
+          <td>{value.description}</td>
+          <td>{value.amount}</td>
+        </tr>);
+    });
+
+    return (
+      <div>
+        <h3>Debts</h3>
+        <table className="table table-inverse">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {debtView}
+          </tbody>
+        </table>
+      </div>)
+  }
+
   renderBills() {
-    return <span>cool</span>;
+    const { Credits, Debts } = this.props.bill;
+
+    return <div>
+             { Credits.length ? this.renderCreditTable() : '' }
+             { Debts.length ? this.renderDebtTable() : '' }
+             { Credits.length === 0 && Debts.length === 0 ?
+               'You currently have no bills. Go ahead and add one!' : '' }
+           </div>;
+  }
+
+  renderLoading() {
+    return <p className="text-xs-center">Loading...</p>
   }
 
   render() {
     return (
       <div>
-        {this.props.bill.isFetching ? 'loading...' : this.renderBills()}
+        {this.props.bill.isFetching ? this.renderLoading() : this.renderBills()}
       </div>
     );
   }
