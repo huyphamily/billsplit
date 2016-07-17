6962e190b85155ea7c14551c1096fb28d11b1a13
huyphamily <huyphamily@gmail.com>
2016-07-17 14:00:54 -0700

    add loading page

--- app/components/MainSection.jsx
+++ app/components/MainSection.jsx
@@ -6,13 +6,11 @@ import 'css/components/main-section';
 const MainSection = () => {
   return (
     <div className={classNames('main-section')}>
-      <div className={classNames('jumbotron', 'jumbotron-fluid')}>
-        <div className={classNames('container', 'text-xs-center')}>
-          <h1 className={classNames('display-4')}>Keep track of bills with friends</h1>
-          <p>
-            <Link className={classNames('btn', 'btn-lg', 'btn-info')} to="/dashboard">Get Started</Link>
-          </p>
-        </div>
+      <div className={classNames('container text-xs-center')}>
+        <h1 className={classNames('display-4')}>Easily keep track of bills with friends</h1>
+        <p>
+          <Link className={classNames('btn btn-lg btn-info')} to="/dashboard">Get Started</Link>
+        </p>
       </div>
     </div>
   );
--- app/containers/LoginOrRegister.jsx
+++ app/containers/LoginOrRegister.jsx
@@ -68,13 +68,6 @@ class LoginOrRegister extends Component {
     );
   }
 
-  // <div class="form-group row">
-  //   <label for="inputEmail3" class="col-sm-2 form-control-label">Email</label>
-  //   <div class="col-sm-10">
-  //     <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-  //   </div>
-  // </div>
-
   render() {
     const { isWaiting, message, isLogin } = this.props.user;
 
